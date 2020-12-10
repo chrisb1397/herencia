661df62ecd7bf3281ca83198d887e0c7cfe57072
ChristianB <christian.benitez@epn.edu.ec>
2020-12-10 10:54:59 -0500

    Insertar persona desde archivo

--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,13 +1,14 @@
 package com.clearminds.model;
 
 import com.clearminds.interfaces.ServicioPersona;
+import com.clearminds.sevicios.ServicioPersonaArchivos;
 import com.clearminds.sevicios.ServicioPersonaBDD;
 
 public class PersonaManager {
 	private ServicioPersona serv;
 	
 	public PersonaManager() {
-		serv = new ServicioPersonaBDD();
+		serv = new ServicioPersonaArchivos();
 	}
 	
 	public void insertarPersona(Persona persona) {
