06e3190ad03a5342cb2208dbf7035dc3104387eb
ChristianB <christian.benitez@epn.edu.ec>
2020-12-10 11:12:53 -0500

    Cambio forma de insertar por medio de classForName

--- src/main/java/com/clearminds/impl/TestManager.java
+++ src/main/java/com/clearminds/impl/TestManager.java
@@ -1,14 +1,21 @@
 package com.clearminds.impl;
 
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.model.Persona;
 import com.clearminds.model.PersonaManager;
 
 public class TestManager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PersonaManager pm = new PersonaManager();
-		pm.insertarPersona(new Persona("Christian", "Benitez", 23));
+		PersonaManager pm;
+		try {
+			pm = new PersonaManager();
+			pm.insertarPersona(new Persona("Christian", "Benitez", 23));
+		} catch (InstanceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,14 +1,22 @@
 package com.clearminds.model;
 
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
-import com.clearminds.sevicios.ServicioPersonaArchivos;
-import com.clearminds.sevicios.ServicioPersonaBDD;
+
 
 public class PersonaManager {
 	private ServicioPersona serv;
 	
-	public PersonaManager() {
-		serv = new ServicioPersonaArchivos();
+	public PersonaManager() throws InstanceException{
+		// serv = new ServicioPersonaArchivos();
+		Class<?> clase;
+		try {
+			clase = Class.forName("com.clearminds.sevicios.ServicioPersonaBDD");
+			serv=(ServicioPersona)clase.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		} 
 	}
 	
 	public void insertarPersona(Persona persona) {
