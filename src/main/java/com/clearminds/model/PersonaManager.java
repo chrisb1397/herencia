3a384c37213d8458882f7959551f3cbb4cde5fd5
ChristianB <christian.benitez@epn.edu.ec>
2020-12-10 11:32:11 -0500

    Datos desde archivo de configuración

--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,5 +1,10 @@
 package com.clearminds.model;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.clearminds.excepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
 
@@ -9,14 +14,32 @@ public class PersonaManager {
 	
 	public PersonaManager() throws InstanceException{
 		// serv = new ServicioPersonaArchivos();
-		Class<?> clase;
+		Class<?> clase;	
+		String servicio = obtenerServicio();
+		
 		try {
-			clase = Class.forName("com.clearminds.sevicios.ServicioPersonaBDD");
+			clase = Class.forName("com.clearminds.sevicios."+servicio);
 			serv=(ServicioPersona)clase.newInstance();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
 		} 
+	}
+	
+	public String obtenerServicio(){
+		String tipoServicio = null;
+		Properties p = new Properties();
+		try {
+			p.load(new FileReader("config.properties"));
+			tipoServicio = p.getProperty("tipoServicio");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			return null;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return null;
+		}
+		return tipoServicio;
 	}
 	
 	public void insertarPersona(Persona persona) {
